fdeab620b5b8e16c7e7fd43b2b5bd50e8df548d8
RamenFornow <almarttala@gmail.com>
2025-04-21 16:29:42 -0500

    Objective List Display
    Rough version of objective list display
    Shows current objectives when escape pressed and objectives are crossed out when completed
    Also minor changes to the level

--- Assets/Scripts/ObjectiveList.cs
+++ Assets/Scripts/ObjectiveList.cs
@@ -2,11 +2,13 @@ using System;
 using System.Collections.Generic;
 using System.Diagnostics;
 using UnityEngine;
+using TMPro;
 
 public class ObjectiveList : MonoBehaviour
 {
     public List<Objective> objectives = new List<Objective>();
-
+    public GameObject pauseMenu;
+    public List<GameObject> objectiveObjectList = new List<GameObject>();
     void Start()
     {
         AddObjective("Meet The Devil.");
@@ -65,15 +67,50 @@ public class ObjectiveList : MonoBehaviour
                 obj.CheckIfCompleted();
                 if (obj.isComplete)
                 {
+                    CompleteObjective(obj.name);
                     UnityEngine.Debug.Log($"Objective Complete: {obj.name}");
                 }
             }
         }
     }
+    void CompleteObjective(string name)
+    { 
+        for (int index = 0; index < objectiveObjectList.Count; index++)
+        {
+            if (objectiveObjectList[index].name == "ObjectiveText" + name)
+            {
+                GameObject tmpObj = objectiveObjectList[index];
+                TextMeshProUGUI tmp = tmpObj.GetComponent<TextMeshProUGUI>();
+                tmp.text = "<s>" + name + "<s>";
+            }
+        }
 
+    }
     public void AddObjective(string name, System.Func<bool> trigger = null, Action onComplete = null)
     {
         Objective newObj = new Objective(name, trigger, onComplete);
         objectives.Add(newObj);
+        // Create a new GameObject
+        int textYPos = 133;
+        int textHeight = 50;
+        int objectiveIndex = objectives.IndexOf(newObj);
+        textYPos = textYPos - (textHeight * objectiveIndex);
+        GameObject tmpObj = new GameObject("ObjectiveText"+name);
+        
+        tmpObj.transform.SetParent(pauseMenu.transform, false);
+        // Set its position in world space
+        //tmpObj.transform.position = new Vector3(0, textYPos, 0);
+
+        // Add TextMeshPro component
+        TextMeshProUGUI tmp = tmpObj.AddComponent<TextMeshProUGUI>();
+
+        tmp.text = name;
+        tmp.fontSize = 36;
+        tmp.color = Color.white;
+        tmp.alignment = TextAlignmentOptions.Center;
+        RectTransform rect = tmp.GetComponent<RectTransform>();
+        rect.anchoredPosition = new Vector2(0, textYPos); // Top center
+        rect.sizeDelta = new Vector2(884, 50);
+        objectiveObjectList.Add(tmpObj);
     }
 }
